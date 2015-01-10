b3222010bd8eaf2dd31dc42890de2527feffc537
Chih-Yung Liang <stu89424@gmail.com>
2015-01-11 04:18:39 +0800

    Finish main

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -2,16 +2,56 @@
 #include "client.h"
 #include "file.h"
 
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 #include <sys/select.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+
 
 TransferringList transferringList;
 
-int main() {
-	int listenFd;
+static int listenFd;
+static void pushAcceptJob();
+static void acceptingClient(void *, ssize_t);
 
-	/* TODO: Initialization */
-	/* TODO: set listenFd */
-	/* TODO: push listen fd into transferList to wait for accept */
+int main(int argc, char *argv[]) {
+	/* TODO: Print Usage */
+#warning "TODO: Print Usage"
+	if(argc != 2) {
+		fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+		exit(1);
+	}
+
+	/* Initialization */
+	File::init();
+
+	/* set listenFd */
+
+	struct sockaddr_in listen_addr;
+	memset(&listen_addr, 0, sizeof(listen_addr));
+	listen_addr.sin_family = AF_INET;
+	listen_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	listen_addr.sin_port = htons(atoi(/* TODO: port arg */argv[1]));
+
+	listenFd = socket(AF_INET, SOCK_STREAM, 0);
+	if(listenFd == -1) {
+		fputs("Cannot create socket.\n", stderr);
+		exit(2);
+	}
+	if(bind(listenFd, (struct sockaddr *) &listen_addr, sizeof(listen_addr)) == -1) {
+		fputs("Cannot bind.\n", stderr);
+		exit(2);
+	}
+	if(listen(listenFd, 1024) == -1) {
+		fputs("Cannot listen.\n", stderr);
+		exit(2);
+	}
+
+	/* push listen fd into transferList to wait for accept */
+	pushAcceptJob();
 
 	fd_set rSet, wSet;
 	int maxFd;
@@ -32,4 +72,20 @@ int main() {
 		Client::checkPullFile();
 		File::checkEachFree();
 	}
+}
+
+void pushAcceptJob() {
+	transferringList.pushReadJob(
+		listenFd,
+		NULL,
+		0,
+		acceptingClient,
+		NULL
+	);
+}
+
+void acceptingClient(void *arg, ssize_t n) {
+	int clientFd = accept4(listenFd, NULL, NULL, SOCK_NONBLOCK);
+	new Client(clientFd);
+	pushAcceptJob();
 }
